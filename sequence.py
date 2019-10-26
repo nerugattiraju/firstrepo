5c1ab167d27c0f04a73e9d8bab52b1677e9bc2bd
ayyappa <nrajubasi@gmail.com>
2019-10-26 17:42:03 +0530

    first commit

---
+++ sequence.py
@@ -0,0 +1,17 @@
+x=[]
+for i in range(int(input())):
+    y=[]
+    name = input()
+    score = float(input())
+    y.append(score)
+    y.append(name)
+    x.append(y)
+sorted(x)
+print(x)
+if x[0][0]==x[1][0]:
+    print(x[0][1])
+    print(x[1][1])
+else:
+    print(x[0][1])
+
+
